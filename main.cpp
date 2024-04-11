84e648b9656dffdbc7071702108e30d3bcdf7574
Luke <lukas.zaborsky@gmail.com>
2024-04-11 11:47:33 +0200

    Register two loaded volumes

--- main.cpp
+++ main.cpp
@@ -13,15 +13,15 @@ int main() {
 	auto fixed = DcmReader(std::filesystem::path("E:") / "VolumeData" / "716" / "716^716_716_CT_2013-04-02_230000_716-1-01_716-1_n81__00000");
 	auto moving = DcmReader(std::filesystem::path("E:") / "VolumeData" / "716" / "716^716_716_CT_2013-04-02_230000_716-2-02_716-2_n81__00000");
 
-	//auto registration = Registration();
-	//registration.SetFixed(fixed.GetOutput());
-	//registration.SetMoving(moving.GetOutput());
-	//auto transform = registration.Register();
+	auto registration = Registration();
+	registration.SetFixed(fixed.GetOutput());
+	registration.SetMoving(moving.GetOutput());
+	auto transform = registration.Register();
 	
 	auto diff = Difference();
 	diff.SetFixed(fixed.GetOutput());
 	diff.SetMoving(moving.GetOutput());
-	//diff.SetTransform(transform);
+	diff.SetTransform(transform);
 	diff.Update();
 
 	auto w = Export();
